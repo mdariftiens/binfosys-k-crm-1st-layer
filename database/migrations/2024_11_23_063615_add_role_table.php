9768b79d6bf2903eb8d58eaae85d4886b658db77
Arif <md.arif.tiens@gmail.com>
2024-11-23 13:35:15 +0600

    add migration

---
+++ database/migrations/2024_11_23_063615_add_role_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id(); // Equivalent to an auto-incrementing UNSIGNED INT
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('permission_type');
+            $table->json('permissions')->nullable();
+            $table->timestamps(); // Adds created_at and updated_at columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
